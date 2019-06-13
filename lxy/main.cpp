05ef15357d7706d638572fecb6bda6c7a78f7e53
lxy2001 <1771478860@qq.com>
2019-06-13 11:54:51 +0800

    jhgyh
    gcvg

--- lxy/main.cpp
+++ lxy/main.cpp
@@ -1,15 +1,24 @@
-//
-//  main.cpp
-//  lxy
-//
-//  Created by s20181106279 on 2019/6/13.
-//  Copyright © 2019 s20181106279. All rights reserved.
-//
 
 #include <iostream>
+#include <cmath>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+class Point{
+public:
+    double Distance(int e,int w,int q,int p){
+        x=pow(q-e,2);
+        s=pow(p-w,2);
+        m=sqrt(x+s);
+        return m;
+    }
+    
+private:
+    double x,s,m;
+};
+int main(){
+    int x1,y2,x2,y1;
+    cin>>x1>>y1>>x2>>y2;
+    Point a;
+    printf("%.2f",a.Distance(x1,y1,x2,y2));
     return 0;
-}
+} 
